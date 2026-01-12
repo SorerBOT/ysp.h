2a535af0a7fc58e855bbac51274abe3c95c2c82f
SorerBOT <alonfiller@gmail.com>
2026-01-13 01:12:00 +0200

    FEAt

--- stack_trace.c
+++ stack_trace.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <dlfcn.h>
 #include <ptrauth.h>
+#include <signal.h>
+#include <sys/time.h>
+
+void print_stack_trace();
 
 void func1();
 void func2();
 void func3();
-void func4();
+size_t func4();
+size_t func5();
 
 void func1()
 {
@@ -25,10 +30,22 @@ void* get_pc()
 {
     return __builtin_return_address(0);
 }
-
-void func4()
+size_t func4()
 {
+    return func5();
+}
+size_t func5()
+{
+    volatile size_t x = 0;
+    for (volatile size_t i = 0; i < 1000 * 1000 * 1000 * 10 ;++i)
+    {
+        x += i;
+    }
+    return x;
+}
 
+void print_stack_trace()
+{
     void* pc_signed = get_pc();
     void* pc = ptrauth_strip(pc_signed, ptrauth_key_return_address);
     Dl_info info = {0};
@@ -53,7 +70,7 @@ void func4()
     {
         printf("rbp = %p\n", rbp);
         printf("rip = %p\n", rip);
-        printf("Current executing function %s\n", info.dli_sname);
+        printf("Called by function %s\n", info.dli_sname);
 
         rbp_signed = *((void**)rbp);
         rbp = ptrauth_strip(rbp_signed, ptrauth_key_frame_pointer);
@@ -66,6 +83,33 @@ void func4()
 
 int main(void)
 {
+    struct sigaction sa = {0};
+    struct itimerval timer;
+
+    // 1. Configure the signal handler
+    sa.sa_handler = print_stack_trace;
+    // Restart system calls if interrupted by signal
+    sa.sa_flags = SA_RESTART; 
+    
+    if (sigaction(SIGPROF, &sa, NULL) == -1) {
+        perror("Error: cannot handle SIGPROF");
+        return 1;
+    }
+
+    // 2. Configure the timer
+    // The timer will expire after 500,000 microseconds (0.5 seconds)
+    timer.it_value.tv_sec = 0;
+    timer.it_value.tv_usec = 500000;
+
+    // And reset to this interval every time it expires
+    timer.it_interval.tv_sec = 0;
+    timer.it_interval.tv_usec = 200;
+
+    // 3. Start the timer using ITIMER_PROF
+    if (setitimer(ITIMER_PROF, &timer, NULL) == -1) {
+        perror("Error: setitimer");
+        return 1;
+    }
     func1();
     return 0;
 }
