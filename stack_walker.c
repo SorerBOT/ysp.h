bd417186e6796c57977e0bab3627dc27e1ea6612
SorerBOT <alonfiller@gmail.com>
2026-01-16 18:12:35 +0200

    FEAT now able to collect samples during runtime, in a way that is signal-safe, and print them afterwards

--- stack_walker.c
+++ stack_walker.c
@@ -4,33 +4,51 @@
 #include <signal.h>
 #include <sys/time.h>
 #include <stdlib.h>
+#include <sys/mman.h>
+
+#define YSP_TIMER_HZ_99_MICRO_SECOND 10101
+
+typedef void ysp_instruction_t;
 
 typedef struct
 {
     size_t depth;
-    const void* instructions[];
+    const ysp_instruction_t* instructions[];
 } ysp_sample_t;
 
-ysp_sample_t* sample = NULL;
+typedef struct
+{
+    size_t samples_offset;
+    size_t samples_count;
+    ysp_sample_t samples[];
+} ysp_profiler_data_t;
+
+ysp_profiler_data_t* profiler = NULL;
 
 void print_stack_trace(int signo, struct __siginfo * siginfo, void * ctx)
 {
+    ysp_sample_t* sample = (ysp_instruction_t*)(((char*)profiler->samples) + profiler->samples_offset);
+    *sample = (ysp_sample_t)
+    {
+        .depth = 0
+    };
+
     ucontext_t* uctx = (ucontext_t*) ctx;
-    void* uctx_rip_signed = (void*) uctx->uc_mcontext->__ss.__pc;
-    void* uctx_rip = ptrauth_strip(uctx_rip_signed, ptrauth_key_return_address);
+    ysp_instruction_t* uctx_rip_signed = (ysp_instruction_t*) uctx->uc_mcontext->__ss.__pc;
+    ysp_instruction_t* uctx_rip = ptrauth_strip(uctx_rip_signed, ptrauth_key_return_address);
 
     sample->instructions[sample->depth] = uctx_rip;
     sample->depth++;
 
-    void* rbp_signed = __builtin_frame_address(0);
-    void* rbp = ptrauth_strip(rbp_signed, ptrauth_key_frame_pointer);
+    ysp_instruction_t* rbp_signed = __builtin_frame_address(0);
+    ysp_instruction_t* rbp = ptrauth_strip(rbp_signed, ptrauth_key_frame_pointer);
 
-    void* rip_signed = *(void**)((char*)rbp + 8);
-    void* rip = ptrauth_strip(rip_signed, ptrauth_key_return_address);
+    ysp_instruction_t* rip_signed = *(ysp_instruction_t**)((char*)rbp + 8);
+    ysp_instruction_t* rip = ptrauth_strip(rip_signed, ptrauth_key_return_address);
 
     while (1)
     {
-        void* calling_rbp_signed = *((void**) rbp);
+        ysp_instruction_t* calling_rbp_signed = *((ysp_instruction_t**) rbp);
         rbp = ptrauth_strip(calling_rbp_signed, ptrauth_key_frame_pointer);
 
         if (rbp == NULL)
@@ -38,28 +56,32 @@ void print_stack_trace(int signo, struct __siginfo * siginfo, void * ctx)
             break;
         }
 
-        void* calling_rip_signed = *(void**)((char*)rbp + 8);
-        void* calling_rip = ptrauth_strip(calling_rip_signed, ptrauth_key_return_address);
+        ysp_instruction_t* calling_rip_signed = *(ysp_instruction_t**)((char*)rbp + 8);
+        ysp_instruction_t* calling_rip = ptrauth_strip(calling_rip_signed, ptrauth_key_return_address);
 
         sample->instructions[sample->depth] = calling_rip;
         sample->depth++;
     }
 
-    for (size_t i = 0; i < sample->depth; ++i)
-    {
-        Dl_info info = (Dl_info) {0};
-        dladdr(sample->instructions[i], &info);
-        printf("func: %s\n", info.dli_sname);
+    profiler->samples_offset += sizeof(ysp_sample_t) + sample->depth * sizeof(ysp_instruction_t*);
+}
+double calc_pi()
+{
+    long long iterations = 1000000; // Needs many iterations!
+    double pi_approx = 0.0;
+    double sign = 1.0; // For alternating signs
+
+    for (long long i = 0; i < iterations; i++) {
+        pi_approx += sign * (4.0 / (2.0 * i + 1.0));
+        sign *= -1.0; // Alternate sign for next term
     }
+
+    return pi_approx;
 }
 
 void shalom3()
 {
-    int x;
-    while (1)
-    {
-        ++x;
-    }
+    printf("PI = %lf\n", calc_pi());
 }
 void shalom2()
 {
@@ -73,11 +95,20 @@ void shalom()
 
 int main(void)
 {
+    profiler = mmap(NULL, 1000 * 1000 * sizeof(char),
+            PROT_READ | PROT_WRITE,
+            MAP_SHARED | MAP_ANON,
+            0, 0);
 
-    sample = malloc(sizeof(ysp_sample_t) + 100 * sizeof(void*));
-    *sample = (ysp_sample_t)
+    if (profiler == NULL)
     {
-        .depth = 0
+        perror("YSP: mmap() error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    *profiler = (ysp_profiler_data_t)
+    {
+        .samples_offset = 0
     };
 
     struct sigaction act = {0};
@@ -85,11 +116,25 @@ int main(void)
     sigaction(SIGPROF, &act, NULL);
 
     struct itimerval time_val = {0};
-    time_val.it_value.tv_sec = 1;
-    time_val.it_interval.tv_sec = 1;
+    time_val.it_value.tv_usec = YSP_TIMER_HZ_99_MICRO_SECOND / 10;
+    time_val.it_interval.tv_usec = YSP_TIMER_HZ_99_MICRO_SECOND / 10;
     setitimer(ITIMER_PROF, &time_val, NULL);
 
     shalom();
+
+    ysp_sample_t* sample = profiler->samples;
+    void* last_addr = ((char*)sample) + profiler->samples_offset;
+    while (sample < (ysp_sample_t*)last_addr)
+    {
+        for (size_t i = 0; i < sample->depth; ++i)
+        {
+            Dl_info info = {0};
+            dladdr(sample->instructions[i], &info);
+            printf("func: %s\n", info.dli_sname);
+        }
+
+        sample = sample + (sizeof(ysp_sample_t) + sample->depth * sizeof(ysp_instruction_t*));
+    }
 
     return 0;
 }
